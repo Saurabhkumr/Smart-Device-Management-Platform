54e7a1a3a8d2f5dc9eb7a5146f8c6e62eb3b6790
Saurabh <saurabhkr0927@gmail.com>
2025-08-19 12:28:40 +0530

    database connection setup

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import pool from "./config/db.js";
 
 dotenv.config();
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+app.get("/", async (req, res) => {
+  const result = await pool.query("SELECT current_database()");
+  res.send(`The database name is : ${result.rows[0].current_database}`);
+});
 
 app.listen(port, () => {
   console.log("server is running on port : ", port);
